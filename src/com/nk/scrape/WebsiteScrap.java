812d2c7b466ffe32c293c89db6c0683f84736f5c
Nikhil <Nikhil@KSHIRSAGAR>
2017-11-20 01:30:21 +0530

    Random changes

--- src/com/nk/scrape/WebsiteScrap.java
+++ src/com/nk/scrape/WebsiteScrap.java
@@ -6,7 +6,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.net.MalformedURLException;
+import java.net.MalformedURLException ; 
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
